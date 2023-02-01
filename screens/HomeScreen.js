987b41f4f7f6307401e74c5a50d54736feb84ae1
Yash Gadhade <yashgadhade@gmail.com>
2023-02-01 13:15:58 +0530

    button alignment

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,73 +1,94 @@
-import React, { useState } from 'react'
-import { TouchableOpacity, StyleSheet, View } from 'react-native'
-import { Text, TextInput } from 'react-native-paper'
-import Button from './Button';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState } from "react";
+import { TouchableOpacity, StyleSheet, View } from "react-native";
+import { Text, TextInput } from "react-native-paper";
+import Button from "./Button";
+import { useNavigation } from "@react-navigation/native";
 
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 // import 'firebase/auth';
 
 export default function HomeScreen(props) {
-
   const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
-        <View style={styles.left}>
-          <Button mode="contained" onPress={() => navigation.navigate("Share Food")} style={styles.default}>
-            Share Food
-          </Button>
-          <Button mode="contained" onPress={() => navigation.navigate("Accept Food")} style={styles.default}>
-            Accept Food
-          </Button>
-          <Button mode="contained" onPress={() => navigation.navigate("Accept Animals")} style={styles.default}>
-            Adopt Animals
-          </Button>
-        </View>
-        <View style={styles.right}>
-          <Button mode="contained" onPress={() => navigation.navigate("Share Clothes")} style={styles.default}>
-            Share Clothes
-          </Button>
-          <Button mode="contained" onPress={() => navigation.navigate("Accept Clothes")} style={styles.default}>
-            Accept Clothes
-          </Button>
-          <Button mode="contained" onPress={() => props.route.params.logout()} style={styles.default}>
+      <View style={styles.left}>
+        <Button
+          mode="contained"
+          onPress={() => navigation.navigate("Share Food")}
+          style={styles.default}
+        >
+          Share Food
+        </Button>
+        <Button
+          mode="contained"
+          onPress={() => navigation.navigate("Accept Food")}
+          style={styles.default}
+        >
+          Accept Food
+        </Button>
+        <Button
+          mode="contained"
+          onPress={() => navigation.navigate("Accept Animals")}
+          style={styles.default}
+        >
+          Adopt Animals
+        </Button>
+      </View>
+      <View style={styles.right}>
+        <Button
+          mode="contained"
+          onPress={() => navigation.navigate("Share Clothes")}
+          style={styles.default}
+        >
+          Share Clothes
+        </Button>
+        <Button
+          mode="contained"
+          onPress={() => navigation.navigate("Accept Clothes")}
+          style={styles.default}
+        >
+          Accept Clothes
+        </Button>
+        <Button
+          mode="contained"
+          onPress={() => props.route.params.logout()}
+          style={styles.default}
+        >
           Logout
         </Button>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   forgotPassword: {
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
     marginBottom: 24,
   },
   left: {
-    flex: 0.4,
+    paddingHorizontal: 2,
   },
   right: {
-    flex: 0.6,
+    paddingHorizontal: 2,
   },
   forgot: {
     fontSize: 13,
     color: "red",
   },
   link: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   default: {
-    backgroundColor: 'blue',
-    width: 200,
+    backgroundColor: "blue",
+    width: 170,
     fontSize: 15,
   },
   container: {
     flexDirection: "row",
-    justifyContent:'center',
-    height: 100,
-    padding: 20,
-    width: '100%',
+    justifyContent: "center",
+    marginTop: 150,
   },
-})
+});
